c2a495d19f066f48c036b21ce1994bb3bf21a7ce
Timon van Spronsen <timonvanspronsen@outlook.com>
2018-01-11 12:17:22 +0100

    Add YouTube editor widget

--- cms/cms.js
+++ cms/cms.js
@@ -5,12 +5,16 @@ import Features from 'site/components/Features'
 import Testimonials from 'site/components/Testimonials'
 import Pricing from 'site/components/Pricing'
 
-import { AboutPageTemplate } from 'site/templates/about-page';
-import { ProductPageTemplate } from 'site/templates/product-page';
-import { BlogPostTemplate } from 'site/templates/blog-post';
+import { AboutPageTemplate } from 'site/templates/about-page'
+import { ProductPageTemplate } from 'site/templates/product-page'
+import { BlogPostTemplate } from 'site/templates/blog-post'
 
-const AboutPagePreview = ({ entry, widgetFor }) =>
-  <AboutPageTemplate title={entry.getIn(['data', 'title'])} content={widgetFor('body')} />;
+const AboutPagePreview = ({ entry, widgetFor }) => (
+  <AboutPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
@@ -30,37 +34,69 @@ const ProductPagePreview = ({ entry, widgetFor, getAsset }) => {
   const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
   const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
-  return <ProductPageTemplate
-    image={entry.getIn(['data', 'image'])}
-    title={entry.getIn(['data', 'title'])}
-    heading={entry.getIn(['data', 'heading'])}
-    description={entry.getIn(['data', 'description'])}
-    intro={{ blurbs }}
-    main={{
-      heading: entry.getIn(['data', 'main', 'heading']),
-      description: entry.getIn(['data', 'main', 'description']),
-      image1: {
-        image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
-        alt: entry.getIn(['data', 'main', 'image1', 'alt']),
-      },
-      image2: {
-        image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
-        alt: entry.getIn(['data', 'main', 'image2', 'alt']),
-      },
-      image3: {
-        image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
-        alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-      },
-    }}
-    fullImage={entry.getIn(['data', 'full_image'])}
-    testimonials={testimonials}
-    pricing={{
-      heading: entry.getIn(['data', 'pricing', 'heading']),
-      description: entry.getIn(['data', 'pricing', 'description']),
-      plans: pricingPlans,
-    }}
-  />;
+  return (
+    <ProductPageTemplate
+      image={entry.getIn(['data', 'image'])}
+      title={entry.getIn(['data', 'title'])}
+      heading={entry.getIn(['data', 'heading'])}
+      description={entry.getIn(['data', 'description'])}
+      intro={{ blurbs }}
+      main={{
+        heading: entry.getIn(['data', 'main', 'heading']),
+        description: entry.getIn(['data', 'main', 'description']),
+        image1: {
+          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image1', 'alt'])
+        },
+        image2: {
+          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image2', 'alt'])
+        },
+        image3: {
+          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image3', 'alt'])
+        }
+      }}
+      fullImage={entry.getIn(['data', 'full_image'])}
+      testimonials={testimonials}
+      pricing={{
+        heading: entry.getIn(['data', 'pricing', 'heading']),
+        description: entry.getIn(['data', 'pricing', 'description']),
+        plans: pricingPlans
+      }}
+    />
+  )
 }
+
+CMS.registerEditorComponent({
+  // Internal id of the component
+  id: 'youtube',
+  // Visible label
+  label: 'Youtube',
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{ name: 'id', label: 'Youtube Video ID', widget: 'string' }],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^youtube (\S+)$/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    }
+  },
+  // Function to create a text block from an instance of this component
+  toBlock: function(obj) {
+    return 'youtube ' + obj.id
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' +
+      obj.id +
+      '/maxresdefault.jpg" alt="Youtube Video"/>'
+    )
+  }
+})
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
